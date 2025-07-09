a5cfc89d4853f28d4194897d65371cddf52f8052
Thiago <bertoldi.thiago1@gmail.com>
2025-07-09 11:10:50 -0300

    feat: Adicionando Dockefile nos workers e configurando docker-compose

--- Queue.Insert/Worker.cs
+++ Queue.Insert/Worker.cs
@@ -3,6 +3,7 @@ using Queue.Domain.Entities;
 using Queue.Repository.Interfaces;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
 using System.Text;
 
 namespace Queue.Insert;
@@ -14,15 +15,33 @@ public class Worker(
     private readonly IRepositoryBase _repository = repository;
     private readonly ILogger<Worker> _logger = logger;
 
-    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken) => await Init(cancellationToken);
+
+    private async Task Init(CancellationToken cancellationToken, int tries = 1)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        using var conn = await factory.CreateConnectionAsync(cancellationToken);
-        using var channel = await conn.CreateChannelAsync(cancellationToken: cancellationToken);
+        try
+        {
+            var factory = new ConnectionFactory { HostName = "rabbitmq" };
+            using var conn = await factory.CreateConnectionAsync(cancellationToken);
+            using var channel = await conn.CreateChannelAsync(cancellationToken: cancellationToken);
 
-        await Processar(channel, "Contact.Queue.Insert", cancellationToken);
+            _logger.LogInformation("Conectado ao RabbitMQ");
 
-        while (!cancellationToken.IsCancellationRequested) { }
+            await Processar(channel, "Contact.Queue.Insert", cancellationToken);
+
+            while (!cancellationToken.IsCancellationRequested) { }
+        }
+        catch (BrokerUnreachableException)
+        {
+            _logger.LogInformation("Falha ao conectar ao RabbitMQ - Tentativa {t}", tries);
+
+            if (tries >= 5)
+                throw new Exception("Erro ao conectar no rabbitMQ, tentativas excedidas");
+
+            Thread.Sleep(10_000);
+
+            await Init(cancellationToken, tries + 1);
+        }
     }
 
     private async Task Processar(IChannel channel, string queue, CancellationToken cancellationToken)
